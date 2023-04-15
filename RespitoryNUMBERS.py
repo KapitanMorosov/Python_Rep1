7e578d4b6ba89b324914f08d1fdba74ff039f103
KapitanMorosov <130863049+KapitanMorosov@users.noreply.github.com>
2023-04-15 12:57:21 -0600

    Add files via upload

---
+++ RespitoryNUMBERS.py
@@ -0,0 +1,14 @@
+x = input("Enter your first value(X): ")
+x = int(x)
+
+y = input("Enter your second value(Y): ")
+y = int(y)
+
+if x > y:
+    print("X is bigger than Y")
+
+if x < y:
+    print("Y is bigger than x")
+
+if x == y:
+    print("The both numbers are equal")
---
+++ RespitoryQUESTIONS.py
@@ -0,0 +1,13 @@
+print("Welcome")
+
+firstN = input("Enter your first name: ")
+lastN = input("Enter your last name: ")
+age = input("Enter your age: ")
+print("                 ")
+gpa = input("Enter your GPA: ")
+major = input("Enter your major: ")
+address = input("Enter your address: ")
+
+
+print(" First name:" + firstN + " ,last name: " + lastN + " ,age: " + age + " GPA: " + gpa + " ,major: " + major + " ,address: " + address)
+
